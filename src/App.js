5f7cea68d8010be9d012c415d46581b888cb264e
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-03-01 02:20:26 +0600

    react router initialized, working fine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Footer from './components/common/Footer/Footer';
 import Header from './components/common/Header/Header';
+import Locations from './components/Locations/Locations';
 import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <main className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="locations" element={<Locations />} />
+
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </main>
   );
 }
 
