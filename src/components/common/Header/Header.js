e6da0930eb08c3a428e2dfec17b83356e096cc3e
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-03-01 02:19:29 +0600

    header created using react bootstarp nav and link added

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="black" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Travel Insider</Navbar.Brand>
+                    <Navbar.Brand href="#home"> <img src="/logo.png" alt="Travel Insider" width="250px" /></Navbar.Brand>
                     <Nav className="ms-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <LinkContainer to="home"><Nav.Link>Home</Nav.Link></LinkContainer>
+                        <LinkContainer to="/features"><Nav.Link>Features</Nav.Link></LinkContainer>
+                        <LinkContainer to="/locations"><Nav.Link>Locations</Nav.Link></LinkContainer>
+                        <LinkContainer to="/pricing"><Nav.Link>Pricing</Nav.Link></LinkContainer>
+                        <LinkContainer to="/login"><Nav.Link>Login</Nav.Link></LinkContainer>
                     </Nav>
                 </Container>
             </Navbar>
