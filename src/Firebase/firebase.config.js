e37e8755244423eac25f24bad99e9edbb4cb72f3
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-03-01 09:44:04 +0600

    new dependencies added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD8aANSmObsDzC7tMc0dYG2vA0zd3MWELA",
+    authDomain: "tourism-sajidmahamud835.firebaseapp.com",
+    projectId: "tourism-sajidmahamud835",
+    storageBucket: "tourism-sajidmahamud835.appspot.com",
+    messagingSenderId: "752882952957",
+    appId: "1:752882952957:web:46d02e381b74ce9144ec94",
+    measurementId: "G-DDNF5PJD07"
+};
+export default firebaseConfig;
