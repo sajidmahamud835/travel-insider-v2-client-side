c4705521857ec5163f268714d87a193d2a0be6d8
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-03-01 09:41:00 +0600

    banner section added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 
 const Home = () => {
+    const sliderHeight = "400px";
     return (
         <div>
-            <h1>This is home</h1>
+            <section id="top-banner" className="">
+                <Carousel fade>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="/images/slider/1.jpg"
+                            height={sliderHeight}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Going For A Vaction?</h3>
+                            <p>Find best travel plan for your vaction and book from home.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="/images/slider/2.jpg"
+                            height={sliderHeight}
+                            alt="Second slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Going For A Vaction?</h3>
+                            <p>Find best travel plan for your vaction and book from home.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="/images/slider/3.jpg"
+                            height={sliderHeight}
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Going For A Vaction?</h3>
+                            <p>Find best travel plan for your vaction and book from home.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </section>
         </div>
     );
 };
